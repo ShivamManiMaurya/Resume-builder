c72ac836f426b8fda21fd3c6e2dc72e950aa27b5
Shivam Maurya <109658264+ShivamManiMaurya@users.noreply.github.com>
2023-06-29 12:25:01 +0530

    ch5 - assignment 1 and 2 done

--- src/otherAssignments/Clock.js
+++ src/otherAssignments/Clock.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Clock = () => {
-  return (
-    <div>Clock</div>
-  )
-}
+    // let sec = 0;
 
-export default Clock
+    const [time, setTime] = useState(0);
+    const [lapsTime, setlapsTime] = useState([]);
+    const [stop, setStop] = useState(true);
+
+    useEffect(() => {
+        let timeout;
+
+        if (!stop) {
+            timeout = setInterval(() => {
+                setTime((val) => val + 10);
+            }, 1);
+        } else if (!time <= 0 && stop) {
+            setlapsTime([...lapsTime, time]);
+            console.log(time);
+        }
+
+        return () => clearInterval(timeout);
+    }, [stop]);
+
+    const formatTime = (timeInMilliseconds) => {
+        const time = new Date(timeInMilliseconds);
+        return time.toISOString().slice(11, -1);
+    };
+
+    const stopStart = () => {
+        setStop((prev) => !prev);
+    };
+
+    return (
+        <div>
+            <h2>{formatTime(time)}</h2>
+            <button onClick={stopStart}>{stop ? "Start" : "Stop"}</button>
+            <button onClick={() => setlapsTime([...lapsTime, time])}>
+                Register
+            </button>
+            <button
+                onClick={() => {
+                    setTime(0);
+                    setlapsTime([]);
+                }}
+            >
+                Reset
+            </button>
+            {lapsTime.map((prevTime, index) => {
+                return <h2 key={index}>{formatTime(prevTime)}</h2>;
+            })}
+        </div>
+    );
+};
+
+export default Clock;
